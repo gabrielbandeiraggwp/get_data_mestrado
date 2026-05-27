8e595b0f5ae78134a7933e9b9b8685fb64bc3796
gabrielbandeiraggwp <gabrielbandeira.ggwp@gmail.com>
2026-05-27 20:20:13 -0300

    20260527

--- dados_1.R
+++ dados_1.R
@@ -1,3 +1,4 @@
+#install.packages(c("rbcb","GetBCBData","dplyr", "ggplot2", "ipeadatar","stargazer", "gridExtra" , "purrr", "lubridate", "mFilter", "tidyverse"))
 library(rbcb)
 #library(BCB)
 library(GetBCBData)
@@ -15,52 +16,80 @@ library(tidyverse)
 
 rm(list = ls())
 
-#dados <- get_series(
-  #c(
-  #  IBC = 24363,
-  #  DIVIDA_LIQUIDA = 4468, #Dívida Líquida do Setor Público - Saldos em R$ milhões - Total - Governo Federal e Banco Central
-  ##  PIB =  4385, #mensal em milhões
-  ##  SELIC = 432,
-  ##  CAMBIO = 1,
-  #  IPCA = 433),
-  #start_date = "2000-01-01",
-  #end_date = "2020-12-30"#
-#)
 
-meta_inflacao <- gbcbd_get_series(13521, first.date = "2000-01-01", last.date = "2024-12-31")
-IBC <- gbcbd_get_series(24363, first.date = "2000-01-01", last.date = "2020-12-31") # disponivel apenas a partir de  2003
-DIVIDA_LIQUIDA <- gbcbd_get_series(4468, first.date = "2000-01-01", last.date = "2020-12-31")
-PIB <- gbcbd_get_series(4380, first.date = "2000-01-01", last.date = "2024-12-31")
-SELIC <- gbcbd_get_series(432, first.date = "2000-01-01", last.date = "2020-12-31")
-CAMBIO <- gbcbd_get_series(1, first.date = "2000-01-01", last.date = "2020-12-31")
-IPCA <- gbcbd_get_series(433, first.date = "2000-01-01", last.date = "2024-12-31")
+meta_inflacao <- gbcbd_get_series(13521, first.date = "2003-01-01", last.date = "2025-12-31")
+IBC <- gbcbd_get_series(24363, first.date = "2003-01-01", last.date = "2024-12-31") # disponivel apenas a partir de  2003
+DIVIDA_LIQUIDA <- gbcbd_get_series(4468, first.date = "2002-01-01", last.date = "2024-12-31")
+PIB <- gbcbd_get_series(4380, first.date = "2002-01-01", last.date = "2024-12-31")
+SELIC <- gbcbd_get_series(432, first.date = "2003-01-01", last.date = "2024-12-31")
+CAMBIO <- gbcbd_get_series(3695, first.date = "2002-09-01", last.date = "2024-12-31")
+IPCA <- gbcbd_get_series(433, first.date = "2003-01-01", last.date = "2024-12-31")
+PIB_INDUSTRIAL <-  ipeadatar::ipeadata("PAN12_QIIGG12")
 EMBI <- ipeadatar::ipeadata("JPM366_EMBI366")
-EXPECTATIVA_INFLACAO <- get_market_expectations(type = "monthly", indicator = "IPCA")
+EXPECTATIVA_INFLACAO <- get_market_expectations(type = "monthly", indic  = "IPCA", start_date = "2003-01-01",
+  end_date = "2024-12-31" )
 #filtrar apenas o ipca e vou escolher a base 0 (antiga metodologia de calculo a nova so tem a partir de  2014)
-EXPECTATIVA_INFLACAO <- EXPECTATIVA_INFLACAO  %>% filter(
-  Indicador == "IPCA", baseCalculo == 0 ,Data > "2000-01-01", Data < "2020-12-31")%>% select(
-    Indicador,DataReferencia,Data,Media, numeroRespondentes, baseCalculo)%>% mutate(
-      DataReferencia = as.Date(paste0("01/", DataReferencia),format = "%d/%m/%Y")) %>% rename(
-        expectativa_ipca_media = Media, ref.date = DataReferencia ) %>% group_by(
-          ref.date, Data)%>% summarise(
-            expectativa_ipca_media = mean(
-              expectativa_ipca_media, na.rm = TRUE))
+#EXPECTATIVA_INFLACAO <- EXPECTATIVA_INFLACAO  %>% filter(
+ # Indicador == "IPCA", baseCalculo == 0 ,Data > "2003-01-01", Data < "2024-12-31")%>% select(
+  #  Indicador,DataReferencia,Data,Media, numeroRespondentes, baseCalculo)%>% mutate(
+  #    DataReferencia = as.Date(paste0("01/", DataReferencia),format = "%d/%m/%Y")) %>% rename(
+  #      expectativa_ipca_media = Media, ref.date = DataReferencia ) %>% group_by(
+   #       ref.date, Data)%>% summarise(
+   #         expectativa_ipca_media = mean(
+      #        expectativa_ipca_media, na.rm = TRUE))
+
+EXPECTATIVA_INFLACAO <- EXPECTATIVA_INFLACAO %>% mutate(#aqui eu criei uma janela entre a data t e a data t+j e filtrei para pegar so o j, que é 6 meses
+  Data = ymd(Data),
+  DataReferencia = my(DataReferencia),
+  Diff_meses = (year(Data) - year(DataReferencia))*12 +
+    (month(Data) - month(DataReferencia))
+) %>% filter(abs(Diff_meses) == 12)
+
+EXPECTATIVA_INFLACAO <- EXPECTATIVA_INFLACAO %>% 
+  mutate(
+    Data = ymd(Data),
+    DataReferencia = ymd(DataReferencia),
+    Data_mes = floor_date(Data, unit ="month"), #transforma qualquer data no inicio daquele mes
+#aqui eu tirei a media com base na data da expectativa e na data da coleta "t"
+  )%>% group_by(Data_mes, DataReferencia) %>% summarise(Media =  mean(Media, na.rm = TRUE), groups = "drop")
+
 
 #cambio e selic não tem dia 1, -> dar um groupby para media
 meta_inflacao <- meta_inflacao %>% rename(meta_inflacao = value
 ) %>% select(meta_inflacao, ref.date
 ) %>% mutate(ano = year(ref.date)
 ) %>% crossing(mes = 1:12
-) %>% mutate(data = as.Date(paste(ano, mes, "01", sep = "-"))
-) %>% arrange(data
-) %>% select(data,  meta_anual = meta_inflacao
-) %>% mutate(meta_inflacao_mensal = ((1 + meta_anual/100)^(1/12) - 1) * 100)
+) %>% mutate(DataReferencia = as.Date(paste(ano, mes, "01", sep = "-"))
+) %>% arrange(DataReferencia
+) %>% select(DataReferencia,  meta_anual = meta_inflacao
+) %>% mutate(meta_inflacao_mensal = ((1 + meta_anual/100)^(1/12) - 1) * 100
+) %>% filter(DataReferencia >= "2003-01-01",DataReferencia < "2026-01-01")
 
-inflacao_menos_meta <- data.frame(desvio_da_meta = IPCA$value - meta_inflacao$meta_inflacao_mensal, data = IPCA$ref.date)
+
+desvio_inflacao_dataframe <- left_join(EXPECTATIVA_INFLACAO, meta_inflacao, by = "DataReferencia") %>% filter(Data_mes >= "2003-01-01", Data_mes < "2025-01-01" ) 
+desvio_inflacao_dataframe <- desvio_inflacao_dataframe %>% mutate(desvios = Media - meta_inflacao_mensal)
+
+DIVIDA_LIQUIDA <- DIVIDA_LIQUIDA %>% rename(DIVIDA_LIQUIDA = value) %>% select("DIVIDA_LIQUIDA", "ref.date") %>% filter(format(ref.date, "%d") == "01")
+DIVIDA_LIQUIDA <- DIVIDA_LIQUIDA %>%
+  mutate(
+    ano = year(ref.date),
+    mes = month(ref.date),
+    # Calcular a dívida do mesmo mês no ano anterior
+    divida_ano_anterior = lag(DIVIDA_LIQUIDA, 12),  # 12 meses atrás
+    # Calcular a variação em pontos percentuais
+    variacao_pp = DIVIDA_LIQUIDA - divida_ano_anterior,
+    # Calcular a variação percentual
+    variacao_percentual = (DIVIDA_LIQUIDA / divida_ano_anterior - 1) * 100
+  ) %>%
+  filter(!is.na(variacao_pp))  # Remover os primeiros 12 meses sem dado anterior
+
+
+
+
 
 
 IBC <- IBC %>% rename(IBC = value) %>% select("IBC", "ref.date") %>% filter(format(ref.date, "%d") == "01")
-DIVIDA_LIQUIDA <- DIVIDA_LIQUIDA %>% rename(DIVIDA_LIQUIDA = value) %>% select("DIVIDA_LIQUIDA", "ref.date") %>% filter(format(ref.date, "%d") == "01") 
+ 
 PIB <- PIB %>% rename(PIB = value) %>% select("PIB", "ref.date")%>% filter(format(ref.date, "%d") == "01")
 IPCA <- IPCA %>% rename(IPCA = value) %>% select("IPCA", "ref.date")%>% filter(format(ref.date, "%d") == "01")
 
@@ -70,6 +99,10 @@ EMBI <- EMBI %>% rename(EMBI = value, ref.date = date) %>%
   group_by(ref.date) %>%
   summarise(EMBI_media= mean(EMBI, na.rm = TRUE))
 
+PIB_INDUSTRIAL <- PIB_INDUSTRIAL %>% rename(PIB_INDUSTRIAL = value, ref.date = date) %>%
+  select("PIB_INDUSTRIAL", "ref.date") %>% 
+  mutate(ref.date = floor_date(ref.date, "month")) %>% filter(ref.date >= "2003-01-01",ref.date < "2025-01-01" )
+
 SELIC <- SELIC %>% filter(format(SELIC$ref.date,"%d") == "01") %>% rename(SELIC = value) %>% select("SELIC", "ref.date")
 #SELIC <- SELIC %>% rename(SELIC = value) %>% #nao sei se é a melhor opção
   #select("SELIC", "ref.date") %>%
@@ -78,26 +111,43 @@ SELIC <- SELIC %>% filter(format(SELIC$ref.date,"%d") == "01") %>% rename(SELIC 
   #summarise(SELIC_media = mean(SELIC, na.rm =TRUE))
 
 
-CAMBIO <- CAMBIO %>% rename(CAMBIO = value) %>%
-  select("CAMBIO", "ref.date") %>%
-  mutate(ref.date= floor_date(ref.date, "month")) %>%
-  group_by(ref.date) %>%
-  summarise(CAMBIO_media = mean(CAMBIO, na.rm = TRUE))
+#CAMBIO <- CAMBIO %>% rename(CAMBIO = value) %>%
+#  select("CAMBIO", "ref.date") %>%
+ # mutate(ref.date= floor_date(ref.date, "month")) %>%
+ # group_by(ref.date) %>%
+ # summarise(CAMBIO_media = mean(CAMBIO, na.rm = TRUE))
+
+CAMBIO <- CAMBIO %>%
+  mutate(
+   retorno_cambio = ((value / lag(value)) - 1)*100
+  ) %>% filter(!is.na(retorno_cambio))
+
+#juros real -> Taxa Real = (1 + Taxa Selic Mensal) / (1 + IPCA Mensal) - 1
+SELIC <- SELIC %>% mutate(
+  selic_fator = 1 + SELIC/100
+)
+IPCA <- IPCA %>% mutate(
+  ipca_fator = 1 + IPCA/100
+)
+SELIC <- SELIC %>% mutate(
+  selic_real = (selic_fator / IPCA$ipca_fator)-1)
 
 
-lista_dfs <- list(IBC,DIVIDA_LIQUIDA,PIB,SELIC,CAMBIO,IPCA,EMBI, EXPECTATIVA_INFLACAO)
-dados_completos <- reduce(lista_dfs, inner_join, by= "ref.date")
+#lista_dfs <- list(IBC,DIVIDA_LIQUIDA,PIB,SELIC,CAMBIO,IPCA,EMBI, EXPECTATIVA_INFLACAO)
+#dados_completos <- reduce(lista_dfs, inner_join, by= "ref.date")
 
 #################################################################
-#  agora vou calcular o hiato do PIB
+#  agora vou calcular o hiato do PIB industrial
 #################################################################
 
 lambda = 14400 # vi que usam esse lambda +para dados mensais
-filtro_hp <- hpfilter(PIB$PIB, freq = lambda)
+filtro_hp <- hpfilter(PIB_INDUSTRIAL$PIB_INDUSTRIAL, freq = lambda)
+
 #dados_completos$tendencia_PIB <- filtro_hp$trend
 #dados_completos$ciclo_PIB <- filtro_hp$cycle
 #dados_completos$hiato <- dados_completos$tendencia_PIB - dados_completos$ciclo_PIB
-hiato <- as.data.frame(filtro_hp$cycle)%>% rename(hiato = `filtro_hp$cycle`)
+hiato <- as.data.frame(filtro_hp$cycle)%>% rename(hiato = `filtro_hp$cycle`) 
+hiato_percentual = hiato$hiato / as.data.frame(filtro_hp$trend) %>% rename(hiato = V1)
 
 #################################################################
 # parte dos graficos#
@@ -105,60 +155,39 @@ hiato <- as.data.frame(filtro_hp$cycle)%>% rename(hiato = `filtro_hp$cycle`)
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = IBC))+
   geom_line( col = "turquoise4") +
-  labs(title = "IBC: 2003 - 2020", x = "Data", y = "IBC")
+  labs(title = "IBC: 2003 - 2024", x = "Data", y = "IBC")
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = DIVIDA_LIQUIDA))+
   geom_line( col = "turquoise4") +
-  labs(title = "DIVIDA LÍQUIDA: 2003 - 2020", x = "Data", y = "DIVIDA LÍQUIDA")
+  labs(title = "DIVIDA LÍQUIDA: 2003 - 2024", x = "Data", y = "DIVIDA LÍQUIDA")
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = PIB))+
   geom_line( col = "turquoise4") +
-  labs(title = "PIB: 2003 - 2020", x = "Data", y = "PIB")
+  labs(title = "PIB: 2003 - 2024", x = "Data", y = "PIB")
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = SELIC))+
   geom_line( col = "turquoise4") +
-  labs(title = "SELIC: 2003 - 2020", x = "Data", y = "SELIC")
+  labs(title = "SELIC: 2003 - 2024", x = "Data", y = "SELIC")
 
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = CAMBIO_media))+
   geom_line( col = "turquoise4") +
-  labs(title = "Média mensal do cambio : 2003 - 2020", x = "Data", y = "CAMBIO")
+  labs(title = "Média mensal do cambio : 2003 - 2024", x = "Data", y = "CAMBIO")
 
 
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = IPCA))+
   geom_line( col = "turquoise4") +
-  labs(title = "IPCA : 2003 - 2020", x = "Data", y = "IPCA")
+  labs(title = "IPCA : 2003 - 2024", x = "Data", y = "IPCA")
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = EMBI_media))+
   geom_line( col = "turquoise4") +
-  labs(title = "Média mensal do EMBI : 2003 - 2020", x = "Data", y = "EMBI")
+  labs(title = "Média mensal do EMBI : 2003 - 2024", x = "Data", y = "EMBI")
 
 ggplot(data = dados_completos, mapping = aes(x = ref.date, y = expectativa_ipca_media))+
   geom_line( col = "turquoise4") +
-  labs(title = "Média mensal da expectativa do IPCA (FOCUS) : 2003 - 2020", x = "Data", y = "EMBI")
+  labs(title = "Média mensal da expectativa do IPCA (FOCUS) : 2003 - 2024", x = "Data", y = "EMBI")
 
-#ggplot(dados_completos, aes(x = ref.date)) +
- # geom_line(aes(y = PIB, color = "PIB")) +
- # geom_line(aes(y = tendencia_PIB, color = "Tendência (HP)")) +
- # labs(title = "PIB vs Tendência (Filtro HP) : 2003 - 2020",
-     #  y = "PIB", x = "Data")
-
-#ggplot(dados_completos, aes(x = ref.date)) +
-  #geom_line(aes(y = tendencia_PIB, color = "Tendência (HP)")) +
-  #geom_line(aes(y = ciclo_PIB, color = "Ciclo")) +
-  #labs(title = "Ciclo vs Tendência do PIB (Filtro HP) : 2003 - 2020",
-     #  y = "PIB", x = "Data")
-
-#ggplot(data = dados_completos, mapping = aes(x = ref.date, y = hiato))+
-  #geom_line( col = "turquoise4") +
-  #labs(title = "Hiato do PIB (filtro hp): 2003 - 2020", x = "Data", y = "Hiato")
-
-#ggplot(dados_completos, aes(x = ref.date)) +
-  #geom_line(aes(y = PIB, color = "PIB")) +
- # geom_line(aes(y = hiato, color = "hiato")) +
-  #labs(title = "PIB vs Hiato do PIB (Filtro HP) : 2003 - 2020",
-   #    y = "PIB", x = "Data")
 
 
 
--- estacionariedade.R
+++ estacionariedade.R
@@ -2,25 +2,61 @@ library(urca)
 
 
 #teste adf e kpss para desvio da inflação
-ggplot(data= inflacao_menos_meta, mapping = aes(x = data, y = desvio_da_meta))+
+ggplot(data= desvio_inflacao_dataframe, mapping = aes(x = Data_mes, y = desvios))+
   geom_line(color = "turquoise4") +
-  labs(title = "Desvios da meta de inflação : 2000 - 2024")
-adf_inflacao <- ur.df(inflacao_menos_meta$desvio_da_meta, type =c("none"), lags = 6, selectlags = "AIC")
-kpss_inflacao <- ur.kpss(inflacao_menos_meta$desvio_da_meta, type = "mu", lags = "long")
+  labs(title = "Desvios da meta de inflação : 2003 - 2024")
+adf_inflacao <- ur.df(desvio_inflacao_dataframe$desvios, type =c("drift"), lags = 6, selectlags = "AIC")
+kpss_inflacao <- ur.kpss(desvio_inflacao_dataframe$desvios, type = "mu", lags = "long")
 summary(adf_inflacao) #estacionaria
 summary(kpss_inflacao) #estacionaria
-sd(inflacao_menos_meta$desvio_da_meta)
-acf(inflacao_menos_meta$desvio_da_meta, plot = TRUE)
-pacf(inflacao_menos_meta$desvio_da_meta, plot = TRUE)
+sd(desvio_inflacao_dataframe$desvios)
+acf(desvio_inflacao_dataframe$desvios, plot = TRUE)
+pacf(desvio_inflacao_dataframe$desvios, plot = TRUE)
 
-#teste adf para hiato do produto
+#teste adf e kpss para hiato do produto
 ggplot(data= hiato, mapping = aes(x = seq_along(hiato), y =hiato))+
   geom_line(color = "turquoise4") +
-  labs(title = "Hiato : 2000 - 2024", y = "HIATO")
-adf_hiato <- ur.df(hiato$hiato, type =c("trend"), lags = 6, selectlags = "AIC")
-kpss_hiato <- ur.kpss(hiato$hiato, type = "tau", lags = "long") #obs^o tau é equvalente a "trend"
+  labs(title = "Hiato : 2003 - 2024", y = "HIATO")
+adf_hiato <- ur.df(hiato_percentual$hiato, type =c("drift"), lags = 6, selectlags = "AIC")
+kpss_hiato <- ur.kpss(hiato_percentual$hiato, type = "mu", lags = "long") #obs^o tau é equvalente a "trend"
 summary(adf_hiato) #aparentemente estacionaria
 summary(kpss_hiato) #estacionaria
-sd(hiato$hiato)
-acf(hiato$hiato, plot = TRUE)
-pacf(hiato$hiato, plot = TRUE)
+sd(hiato_percentual$hiato)
+acf(hiato_percentual$hiato, plot = TRUE)
+pacf(hiato_percentual$hiato, plot = TRUE)
+
+#teste adf e kpss para cambio
+ggplot(data= CAMBIO, mapping = aes(x = ref.date, y = retorno_cambio))+
+  geom_line(color = "turquoise4") +
+  labs(title = "Taxa de câmbio - Livre - Dólar americano (compra) ", y = "Fim de período - mensal : 2003 - 2024")
+adf_cambio <- ur.df(CAMBIO$retorno_cambio, type =c("drift"), lags = 6, selectlags = "AIC")
+kpss_cambio <- ur.kpss(CAMBIO$retorno_cambio, type = "mu", lags = "long") #obs^o tau é equvalente a "trend"
+summary(adf_cambio)
+summary(kpss_cambio) 
+sd(CAMBIO$retorno_cambio)
+acf(CAMBIO$retorno_cambio, plot = TRUE)
+pacf(CAMBIO$retorno_cambio, plot = TRUE)
+
+#teste adf e kpss para o EMBI
+ggplot(data= EMBI, mapping = aes(x = ref.date, y = EMBI_media))+
+  geom_line(color = "turquoise4") +
+  labs(title = "EMBI : 2003 - 2024", y = "EMBI")
+adf_EMBI <- ur.df(EMBI$EMBI_media, type =c("drift"), lags = 6, selectlags = "AIC")
+kpss_EMBI <- ur.kpss(EMBI$EMBI_media, type = "tau", lags = "long") #obs^o tau é equvalente a "trend"
+summary(adf_EMBI) 
+summary(kpss_EMBI)
+sd(EMBI$EMBI_media)
+acf(EMBI$EMBI_media, plot = TRUE)
+pacf(EMBI$EMBI_media, plot = TRUE)
+
+#teste adf e kpss para a dívida líquida do setor publico
+ggplot(data= DIVIDA_LIQUIDA, mapping = aes(x = ref.date, y = variacao_percentual))+
+  geom_line(color = "turquoise4") +
+  labs(title = "Dívida Líquida : 2003 - 2024", y = "Divida Líquida")
+adf_divida <- ur.df(DIVIDA_LIQUIDA$variacao_percentual, type =c("trend"), lags = 6, selectlags = "AIC")
+kpss_divida <- ur.kpss(DIVIDA_LIQUIDA$variacao_percentual, type = "mu", lags = "long") #obs^o tau é equvalente a "trend"
+summary(adf_divida) #aparentemente estacionaria
+summary(kpss_divida) #estacionaria
+sd(DIVIDA_LIQUIDA$variacao_percentual)
+acf(DIVIDA_LIQUIDA$variacao_percentual, plot = TRUE)
+pacf(DIVIDA_LIQUIDA$variacao_percentual, plot = TRUE)
